d63fb8a18e5a02085b7cb5981abbb8774b35f6a8
Muhammad Fadila <muh.fadila18@gmail.com>
2022-11-11 17:20:54 +0700

    add public void addNote

--- Program.cs
+++ Program.cs
@@ -18,5 +18,18 @@ namespace single_linked_list
         {
             START = null;
         }
+        public void addNote() //add a node in the list
+        {
+            int nim;
+            string nm;
+            Console.Write("\nEnter the roll number of the student :  ");
+            nim = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nEnter the name of the student :  ");
+            nm = Console.ReadLine();
+            Node newnode = new Node();
+            newnode.rollNumber = nim;
+            newnode.name = nm;
+            // if the node to be inserted is the first node
+        }
     }
 }
